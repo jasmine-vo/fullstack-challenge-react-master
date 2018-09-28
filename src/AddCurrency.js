e268e612a38397433615f1a4d30e1c57b0363e2c
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-09-28 11:19:10 -0700

    convert AddCurrency component to a class component

--- src/AddCurrency.js
+++ src/AddCurrency.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function AddCurrency () {
+class AddCurrency extends Component {
 // to do: min char 2/disable button, max char limit 6, add listener
-  return (
-    <form>
-      <label className="label">
-        Add Cryptocurrency
-      </label>
+  render() {
+    return (
+      <form>
+        <label className="label">
+          Add Cryptocurrency
+        </label>
 
-      <input
-        className="input is-medium is-inline"
-        type="text"
-        placeholder="Symbol (ex. BTC)..."
-      />
+        <input
+          className="input is-medium is-inline"
+          type="text"
+          placeholder="Symbol (ex. BTC)..."
+        />
 
-      <input
-        type="submit"
-        value="Save"
-        className="button is-medium"
-      />
-    </form>
-  );
+        <input
+          type="submit"
+          value="Save"
+          className="button is-medium"
+        />
+      </form>
+    );
+  }
 }
 
 export default AddCurrency;
