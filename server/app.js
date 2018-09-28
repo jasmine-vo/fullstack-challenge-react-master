881b7d938c463409e72f5baffd2a74177fff6628
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-09-27 22:15:28 -0700

    add GET request to fetch current cryptocurrency prices

--- server/app.js
+++ server/app.js
@@ -1,5 +1,26 @@
-const app = require('express')()
-const firebase = require('./firebase')
-const port = 5000
+const app = require('express')();
+const firebase = require('./firebase');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const port = 5000;
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+app.get('/currencies', (req, res) => {
+  firebase.database().ref('currencies/').once('value')
+    .then(function(snapshot) {
+      const currencies = Object.keys(snapshot.val()).join(',');
+
+    fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${currencies}&tsyms=USD`)
+      .then(res => res.json())
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => console.error(err));
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
