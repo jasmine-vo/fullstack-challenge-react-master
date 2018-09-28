a59d4880b44ed39207e72fc5c458b7b00324480b
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-09-28 11:28:13 -0700

    add handleChange and handleSubmit methods

--- src/AddCurrency.js
+++ src/AddCurrency.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 
 class AddCurrency extends Component {
 // to do: min char 2/disable button, max char limit 6, add listener
+  state = {
+    symbol: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({ symbol: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.symbol)
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label className="label">
           Add Cryptocurrency
         </label>
@@ -13,6 +26,7 @@ class AddCurrency extends Component {
           className="input is-medium is-inline"
           type="text"
           placeholder="Symbol (ex. BTC)..."
+          onChange={this.handleChange}
         />
 
         <input
